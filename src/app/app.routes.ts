41d814c0b96955b85a5c30d164ef90edf647dbf2
Jorgito170 <jorgeenrique7900@gmail.com>
2025-07-03 16:57:26 -0500

    fix: correccion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './public/pages/home/home.component';
+import { ProfileComponent } from './public/pages/profile/profile.component';
+import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
--- src/app/public/pages/profile/profile.component.ts
+++ src/app/public/pages/profile/profile.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
   imports: [
-    FormsModule
+    FormsModule,
+    NgIf
   ]
 })
 export class ProfileComponent {
