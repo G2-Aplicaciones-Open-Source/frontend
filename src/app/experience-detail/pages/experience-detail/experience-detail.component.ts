beea991528f8f7ca238fa83576dad2164f861c44
Farid Sebastian Briceño De La Cruz <thelynx412@gmail.com>
2025-07-05 18:31:24 -0500

    update

--- src/app/experience-detail/pages/experience-detail/experience-detail.component.ts
+++ src/app/experience-detail/pages/experience-detail/experience-detail.component.ts
@@ -4,11 +4,15 @@ import { ExperienceService, Experience } from '../../services/experience.service
 import { ActivatedRoute } from '@angular/router';
 import {AvailabilityCheckerComponent} from '../../components/availability-checker/availability-checker.component';
 import {CommonModule} from '@angular/common';
+import {ReviewListComponent} from '../../components/review-list/review-list.component';
+import {FavoriteService} from '../../services/favorite.service';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-experience-detail',
   standalone: true,
-  imports: [CommonModule, AvailabilityCheckerComponent],
+  imports: [CommonModule, AvailabilityCheckerComponent, ReviewListComponent, MatIcon, MatIconButton],
   templateUrl: './experience-detail.html',
   styleUrls: ['./experience-detail.scss']
 })
@@ -18,9 +22,13 @@ export class ExperienceDetailComponent implements OnInit {
   isLoading = true;
   showAvailability = false;
 
+  isFavorite = false;
+  userId: number = JSON.parse(localStorage.getItem('user') || '{}').profileId || 1; // Default to 1 if not found
+
   constructor(
     private route: ActivatedRoute,
-    private experienceService: ExperienceService
+    private experienceService: ExperienceService,
+    private favoriteService: FavoriteService
   ) {}
 
   ngOnInit(): void {
@@ -34,9 +42,26 @@ export class ExperienceDetailComponent implements OnInit {
         this.isLoading = false;
       }
     });
+
+    this.favoriteService.getByUser(this.userId).subscribe(favs => {
+      this.isFavorite = favs.some(f => f.experienceId === this.experienceId);
+    });
   }
 
   toggleAvailability(): void {
     this.showAvailability = !this.showAvailability;
   }
+
+  toggleFavorite(): void {
+    if (this.isFavorite) {
+      this.favoriteService.remove(this.userId, this.experienceId).subscribe(() => {
+        this.isFavorite = false;
+      });
+    } else {
+      this.favoriteService.add({ userId: this.userId, experienceId: this.experienceId }).subscribe(() => {
+        this.isFavorite = true;
+      });
+    }
+  }
+
 }
