1ed388eda3edd6d6ea2f096f5ea01d132aa56b9d
Farid Sebastian Briceño De La Cruz <thelynx412@gmail.com>
2025-07-11 03:02:26 -0500

    update experience cars in home

--- src/app/public/components/experience-card/experience-card.component.ts
+++ src/app/public/components/experience-card/experience-card.component.ts
@@ -1,23 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { Experience } from '../../../shared/models/experience.model';
-import {NgIf} from '@angular/common';
-import {TranslatePipe} from '@ngx-translate/core';
+import { NgIf } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-experience-card',
   templateUrl: './experience-card.component.html',
   styleUrls: ['./experience-card.component.scss'],
-  imports: [
-    NgIf,
-    TranslatePipe
-  ]
+  standalone: true,
+  imports: [NgIf, TranslatePipe]
 })
 export class ExperienceCardComponent {
   @Input() experience!: Experience;
   isFavorite = false;
 
+  constructor(private router: Router) {}
+
   toggleFavorite() {
     this.isFavorite = !this.isFavorite;
     // Aquí luego podrás emitir un evento o llamar a un servicio para persistir favoritos.
+  }
+
+  goToDetails() {
+    this.router.navigate(['/experience-detail', this.experience.id]);
   }
 }
